63a9ddf0b536b871ee98a105e01d8e34443a860c
sofiekhullar <sofie_k@live.se>
2017-06-07 14:43:28 +0900

    small fixes

--- dictionary.php
+++ dictionary.php
@@ -2,8 +2,6 @@
     
     $wordArray = explode(" ", $_REQUEST['q']);
 
-    //echo $_POST['word'] . "<br><br>";
-
     foreach ($wordArray as $word ) {
 
       $wordType = file_get_contents("http://www.dictionaryapi.com/api/v1/references/collegiate/xml/" . $word . "?key=496cb26a-f82d-4773-abe1-f3fa5936a00d");
