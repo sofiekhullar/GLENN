fcaa68aa60ac885fe34c4a15af7d2bb6a9951fd0
sofiekhullar <sofie_k@live.se>
2017-06-07 13:49:18 +0900

    Recipe now working

--- dictionary.php
+++ dictionary.php
@@ -2,13 +2,13 @@
     
     $wordArray = explode(" ", $_REQUEST['q']);
 
-    echo $_POST['word'] . "<br><br>";
+    //echo $_POST['word'] . "<br><br>";
 
     foreach ($wordArray as $word ) {
 
       $wordType = file_get_contents("http://www.dictionaryapi.com/api/v1/references/collegiate/xml/" . $word . "?key=496cb26a-f82d-4773-abe1-f3fa5936a00d");
       $xml = simplexml_load_string($wordType);
-
-      echo $word . " is a " . $xml->entry->fl . "<br>";
+      	if($xml->entry->fl == "noun" && $word != "recipe")
+      		echo $word . " ";
     }
 ?>
